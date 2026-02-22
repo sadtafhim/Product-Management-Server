93e02592549cd023bf1079653d4ba81c8a77bfbf
Saad <tafhim.sad@gmail.com>
2026-02-22 11:13:32 +0600

    second commit

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
-
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
 const uri = process.env.MONGO_URI;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,49 +18,45 @@ const client = new MongoClient(uri, {
   },
 });
 
-let db, productCollection;
+app.get("/", (req, res) => {
+  res.send("server is running!");
+});
 
-async function connectDB() {
-  if (!db) {
+async function run() {
+  try {
     await client.connect();
-    db = client.db("product_manager");
-    productCollection = db.collection("products");
+    const db = client.db("product_manager");
+    const productCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const product_data = req.body;
+      const result = await productCollection.insertOne(product_data);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const result = await productCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+    app.put("/products/:id", async (req, res) => {
+      const result = await productCollection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body },
+      );
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
   }
 }
-
-app.get("/", (req, res) => res.send("Server is running!"));
-
-app.get("/products", async (req, res) => {
-  await connectDB();
-  const result = await productCollection.find().toArray();
-  res.send(result);
-});
-
-app.post("/products", async (req, res) => {
-  await connectDB();
-  const result = await productCollection.insertOne(req.body);
-  res.send(result);
-});
-
-app.delete("/products/:id", async (req, res) => {
-  await connectDB();
-  const result = await productCollection.deleteOne({
-    _id: new ObjectId(req.params.id),
-  });
-  res.send(result);
-});
-
-app.put("/products/:id", async (req, res) => {
-  await connectDB();
-  const result = await productCollection.updateOne(
-    { _id: new ObjectId(req.params.id) },
-    { $set: req.body },
-  );
-  res.send(result);
-});
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(5000, () => console.log("Local server on 5000"));
-}
+run().catch(console.dir);
 
 module.exports = app;
